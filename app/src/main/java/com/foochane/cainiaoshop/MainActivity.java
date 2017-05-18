0258dbe84603f144a6e1be1a5d329d45c024f5a2
foochane <foochengkm@163.com>
2017-05-18 13:23:47 +0800

    添加第一个fragment

--- app/src/main/java/com/foochane/cainiaoshop/MainActivity.java
+++ app/src/main/java/com/foochane/cainiaoshop/MainActivity.java
@@ -1,13 +1,45 @@
 package com.foochane.cainiaoshop;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TextView;
 
+import com.foochane.cainiaoshop.fragment.HomeFragment;
+import com.foochane.cainiaoshop.widget.FragmentTabHost;
+
+
+//Activity要继承FragmentActivity
+//AppCompatActivity已经继承了FragmentActivity
 public class MainActivity extends AppCompatActivity {
+
+    private LayoutInflater mInflater;
+
+    private FragmentTabHost mTabhost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mInflater = LayoutInflater.from(this);
+
+        mTabhost = (FragmentTabHost) this.findViewById(android.R.id.tabhost);
+        mTabhost.setup(this,getSupportFragmentManager(),R.id.realtabcontent); //不是tabcontent
+
+
+        View view = mInflater.inflate(R.layout.tab_indicator,null);
+        ImageView img = (ImageView) view.findViewById(R.id.icon_tab);
+        TextView text = (TextView) view.findViewById(R.id.txt_indicator);
+
+        img.setBackgroundResource(R.mipmap.icon_home);
+        text.setText("主页");
+
+        TabHost.TabSpec  tabSpec = mTabhost.newTabSpec("home");
+        tabSpec.setIndicator(view);
+        mTabhost.addTab(tabSpec , HomeFragment.class,null);
     }
 }
---
+++ app/src/main/java/com/foochane/cainiaoshop/fragment/HomeFragment.java
@@ -0,0 +1,12 @@
+package com.foochane.cainiaoshop.fragment;
+
+
+//注意：不是import android.app.Fragment;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by foochane on 2017/5/18 0018.
+ */
+
+public class HomeFragment extends Fragment {
+}
