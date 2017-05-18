e0bc7f1caf91ab801bc4efbca7ddbcceebc3caf0
foochane <foochengkm@163.com>
2017-05-18 15:32:40 +0800

    fragmentTabHost 实现底部菜单.

--- app/src/main/java/com/foochane/cainiaoshop/MainActivity.java
+++ app/src/main/java/com/foochane/cainiaoshop/MainActivity.java
@@ -5,11 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TabHost;
 import android.widget.TextView;
 
+import com.foochane.cainiaoshop.bean.Tab;
+import com.foochane.cainiaoshop.fragment.CartFragment;
+import com.foochane.cainiaoshop.fragment.CategoryFragment;
 import com.foochane.cainiaoshop.fragment.HomeFragment;
+import com.foochane.cainiaoshop.fragment.HotFragment;
+import com.foochane.cainiaoshop.fragment.MineFragment;
 import com.foochane.cainiaoshop.widget.FragmentTabHost;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 //Activity要继承FragmentActivity
@@ -20,26 +29,64 @@ public class MainActivity extends AppCompatActivity {
 
     private FragmentTabHost mTabhost;
 
+    private List<Tab> mTabs = new ArrayList<>(5);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initTab();
+
+    }
+
+    private void initTab() {
+
+
+        Tab tab_home = new Tab(HomeFragment.class,R.string.home,R.drawable.selector_icon_home);
+        Tab tab_hot = new Tab(HotFragment.class,R.string.hot,R.drawable.selector_icon_hot);
+        Tab tab_category = new Tab(CategoryFragment.class,R.string.catagory,R.drawable.selector_icon_category);
+        Tab tab_cart = new Tab(CartFragment.class,R.string.cart,R.drawable.selector_icon_cart);
+        Tab tab_mine = new Tab(MineFragment.class,R.string.mine,R.drawable.selector_icon_mine);
+
+        mTabs.add(tab_home);
+        mTabs.add(tab_hot);
+        mTabs.add(tab_category);
+        mTabs.add(tab_cart);
+        mTabs.add(tab_mine);
+
+
+
         mInflater = LayoutInflater.from(this);
-
         mTabhost = (FragmentTabHost) this.findViewById(android.R.id.tabhost);
-        mTabhost.setup(this,getSupportFragmentManager(),R.id.realtabcontent); //不是tabcontent
+        mTabhost.setup(this,getSupportFragmentManager(),R.id.realtabcontent);
+
+        for (Tab tab : mTabs){
+
+            TabHost.TabSpec tabSpec = mTabhost.newTabSpec(getString(tab.getTitle()));
+
+            tabSpec.setIndicator(buildIndicator(tab));
+
+            mTabhost.addTab(tabSpec,tab.getFragment(),null);
+
+        }
 
 
-        View view = mInflater.inflate(R.layout.tab_indicator,null);
+
+
+    }
+
+
+    private  View buildIndicator(Tab tab){
+
+
+        View view =mInflater.inflate(R.layout.tab_indicator,null);
         ImageView img = (ImageView) view.findViewById(R.id.icon_tab);
         TextView text = (TextView) view.findViewById(R.id.txt_indicator);
 
-        img.setBackgroundResource(R.mipmap.icon_home);
-        text.setText("主页");
+        img.setBackgroundResource(tab.getIcon());
+        text.setText(tab.getTitle());
 
-        TabHost.TabSpec  tabSpec = mTabhost.newTabSpec("home");
-        tabSpec.setIndicator(view);
-        mTabhost.addTab(tabSpec , HomeFragment.class,null);
+        return  view;
     }
 }
